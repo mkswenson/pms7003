85b1a489367a1bd9fb2984615169938b8c154614
Kjetil Orbekk <kj@orbekk.com>
2021-11-12 18:39:38 -0500

    Add explicit nixpkgs path

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   };
 
   outputs = { self, nixpkgs, utils, naersk }:
