857ee8d49847bd97c6049a10a46b7fe956db074a
Kjetil Orbekk <kj@orbekk.com>
2022-10-30 11:27:14 -0400

    Update cargo dependencies and rust toolchain

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
   };
 
   outputs = { self, nixpkgs, utils, naersk }:
